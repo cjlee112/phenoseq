73ed0447f28d6379fdf47753dffc0e83f9f176d2
Christopher Lee <leec@chem.ucla.edu>
2010-09-07 15:44:45 -0700

    add sample_maxhit() for calculating real target hit probabilities

--- basic_stats.py
+++ basic_stats.py
@@ -1,5 +1,8 @@
 
 import random
+import numpy
+from scipy import stats
+from math import exp, log
 
 def sample_genes(ngene=4200, nmut=100):
     """Generate counts of genes with 1, 2 or more mutations,
@@ -12,4 +15,55 @@ def sample_genes(ngene=4200, nmut=100):
     for i in d.values():
         c[i] = c.get(i, 0) + 1
     return c
+
+def waiting_times(n, mu):
+    'generates infinite sequence of exponential waiting times'
+    pdf = stats.expon(0, mu)
+    i = 0
+    data = pdf.rvs(n)
+    while True:
+        try:
+            yield data[i]
+        except IndexError:
+            i = 0
+            data = pdf.rvs(n)
+            yield data[i]
+        i += 1
+
+def sample_maxhit(n, nstrain, ntarget, ngene, lam):
+    '''Compute fraction of best hits that are real targets in
+    multihit screen, using probability of hitting target
+    gene(s) conditioned on requiring at least one such hit.
+    n: number of replicates
+    nstrain: number of strains to use for each replicate
+    ntarget: number of target genes that cause the phenotype
+    ngene: number of total genes in the genome
+    lam: the Poisson lambda parameter representing mean number of
+    mutations per gene.  E.g. for 50 mutations in 4000 genes
+    lam = 50./4000'''
+    mu = 1. / lam # mean waiting time parameter
+    f = 1. - exp(-ntarget / mu)
+    r = numpy.random.random_sample(n * nstrain) # random x from 0 - 1
+    start = (-mu) * numpy.log(1. - f * r) # location of 1st target mutation
+    waitTime = waiting_times(2. * n * nstrain * ntarget * lam, 1. / lam)
+    pois = stats.poisson(nstrain * lam)
+    istart = 0
+    ngood = nok = 0
+    for i in xrange(n):
+        realhits = {}
+        for j in xrange(nstrain):
+            x = start[istart]
+            istart += 1
+            while x < ntarget: # record target mutations in this strain
+                k = int(x)
+                realhits[k] = realhits.get(k, 0) + 1
+                x += waitTime.next()
+        nontargets = pois.rvs(ngene - ntarget) # #hits per gene
+        maxreal = max(realhits.values())
+        maxbad = nontargets.max()
+        if maxreal > maxbad: # real target hit more than any non-target
+            ngood += 1
+        elif maxreal == maxbad: # real target tied with best non-target
+            nok += 1
+    return float(ngood) / n, float(ngood + nok) / n
 
