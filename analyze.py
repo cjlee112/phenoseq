84951601e9f3165ea24ff794cc24a41edf4652c1
Christopher Lee <leec@chem.ucla.edu>
2010-09-29 19:57:18 -0700

    added ReplicateSet and TagDict functionality, simple usage example

--- analyze.py
+++ analyze.py
@@ -1,11 +1,22 @@
 from scipy import stats
 from math import log
+import glob
+
+''' simple usage example:
+
+laptop:doe leec$ python2.6 -i analyze.py [ACGT]*.vcf
+>>> len(tagDict)
+10
+>>> sum([len(list(rs)) for rs in tagDict.values()])/32.
+128.09375
+
+'''
 
 def convert_number(s):
     'convert string to number if possible'
     if not s[0].isdigit():
         return s
-    elif s.find('.') >= 0:
+    elif s.find('.') >= 0 or s.lower().find('e') >= 0:
         return float(s)
     else:
         return int(s)
@@ -81,3 +92,53 @@ def get_binom_cutoff(c, p, s, nmax=1):
 def binom_filter(snp, p=0.01, s=4e+06):
     'return True if snp passes sequencing error cutoff'
     return snp.nalt >= get_binom_cutoff(snp.nreads, p, s)
+
+class ReplicateSet(object):
+    'filters SNP candidates using replicate lanes'
+    def __init__(self, mergedFile, replicateFiles, minRep=2, maxF=0.5):
+        self.snps = read_vcf(mergedFile, add_attrs=add_snp_attrs)
+        self.minRep = minRep
+        self.maxF = maxF
+        self.replicates = []
+        d = {}
+        for path in replicateFiles: # read replicate lanes
+            snps = read_vcf(path, add_attrs=add_snp_attrs)
+            self.replicates.append(snps)
+            for snp in snps: # index SNP records from replicate lanes
+                d.setdefault((snp.pos, snp.alt), []).append(snp)
+        self.repMap = d
+
+    def __getitem__(self, snp):
+        'get repMap records matching this snp pos and letter'
+        return self.repMap.get((snp.pos, snp.alt), ())
+
+    def __iter__(self):
+        'generate all snps matching filter criteria'
+        for snp in self.snps:
+            if snp.af1 <= self.maxF and len(self[snp]) >= self.minRep:
+                yield snp
+
+
+class TagDict(dict):
+    'keys are tags, values are ReplicateSet objects'
+    def __init__(self, tagDict):
+        dict.__init__(self)
+        d = {}
+        for tag,args in tagDict.items():
+            self[tag] = ReplicateSet(*args)
+
+def read_tag_files(tagFiles, tagfunc=lambda s:s.split('.')[0],
+                      replicatefunc=lambda s:glob.glob('*_' + s + '.vcf'),
+                      *args):
+    'construct TagDict for all these tagFiles'
+    d = {}
+    for tagFile in tagFiles:
+        tag = tagfunc(tagFile)
+        repFiles = replicatefunc(tag)
+        d[tag] = (tagFile, repFiles) + args
+    return TagDict(d)
+
+if __name__ == '__main__':
+    import sys
+    tagFiles = sys.argv[1:]
+    tagDict = read_tag_files(tagFiles)
