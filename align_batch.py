f3987e3f6df4d99615c01ca50ab45e0f5094e60a
Christopher Lee <leec@chem.ucla.edu>
2011-01-25 17:36:00 -0800

    avoid using deprecated shell=True arg for subprocess.call()

--- align_batch.py
+++ align_batch.py
@@ -17,6 +17,11 @@ if __name__ == '__main__':
         # Skip the file of undecipherable reads.
         if tag == "other":
             continue
-        cmd = "%s -d %s -o SAM -f s_%s_%s_qseq.txt > aligned_s_%s_%s.sam" % (novoalign_executable, index_filename, lane, tag, lane, tag)
+        cmd = [novoalign_executable, '-d', index_filename, '-o', 'SAM',
+               '-f', 's_%s_%s_qseq.txt' % (lane, tag)]
         print cmd
-        subprocess.call(cmd, shell=True)
+        outputFile = open('aligned_s_%s_%s.sam' % (lane, tag), 'w')
+        try:
+            subprocess.check_call(cmd, stdout=outputFile)
+        finally:
+            outputFile.close()
