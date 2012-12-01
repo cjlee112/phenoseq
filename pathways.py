f144f5aa0fdfa2359efd465f1f0ec77e11bb7de6
Christopher Lee <leec@chem.ucla.edu>
2012-11-30 19:07:42 -0800

    deleted redundant pathways code

--- pathways.py
+++ pathways.py
@@ -29,6 +29,7 @@ from phenoseq.analyze import *
             ##pathways.append(p)
     #return pathway_dict
 
+
 def load_gene_translations(filename="16.1/data/genes.col"):
     """Loads Ecocyc genes file. Needed to process other files (to get the correct gene names). See example at: http://bioinformatics.ai.sri.com/ptools/flatfile-samples/genes.col"""
     #pathways = []
@@ -51,6 +52,7 @@ def load_gene_translations(filename="16.1/data/genes.col"):
             d[split[1]] = split[2]
     return d    
     
+
 def load_func_assoc(filename="16.1/data/func-associations.col"):
     """Loads Ecocyc functional associations file. See example at: http://bioinformatics.ai.sri.com/ptools/flatfile-samples/func-associations.col"""
     pathway_dict = dict()
@@ -87,64 +89,6 @@ def load_func_assoc(filename="16.1/data/func-associations.col"):
             #pathways.append(p)
     return pathway_dict    
 
-def load_data(tagFiles, count_syn=True):
-    annodb, al, dna = read_genbank_annots("NC_000913.gbk")
-    snps = read_tag_files(tagFiles)
-    #gsd = GeneSNPDict(tagDict, annodb, al, dna, count_syn=count_syn)
-    gsd = map_snps_chrom1(snps, al, dna)
-    return (annodb, al, dna, snps, gsd)    
-
-def count_snps_per_gene(snps, al, dna):
-    """Counts snps for each gene."""
-    rc = dict(A='T', C='G', G='C', T='A')
-    snp_counts = {}
-    for snp in snps:
-        pos = snp.pos
-        try:
-            geneSNP = al[dna[pos - 1:pos]].keys()[0]
-        except IndexError:
-            continue
-        else:
-            Na = 0
-            Ns = 0
-            # Determine if snp is synonymous or not.
-            if geneSNP.orientation < 0:
-                geneSNP = -geneSNP
-            gene = geneSNP.id
-            ipos = geneSNP.start % 3
-            codonStart = geneSNP.start - ipos
-            codon = str(geneSNP.path.sequence[codonStart:codonStart + 3])
-            b = snp.alt # substitution letter
-            if geneSNP.sequence.orientation < 0: # must complement
-                b = rc[b.upper()]
-            codonAlt = codon[:ipos] + b + codon[ipos + 1:]
-            snp.aaRef = sequtil.translate_orf(codon)
-            snp.aaAlt = sequtil.translate_orf(codonAlt)
-            if snp.aaRef == snp.aaAlt:
-                Ns += 1
-            else:
-                Na += 1
-        try:
-            a, b = snp_counts[gene]
-            Na += a
-            Ns += b
-        except KeyError:
-            pass
-        snp_counts[gene] = (Na, Ns)
-    return snp_counts
-
-def count_snps_per_pathway(snp_counts, pathway_dict):
-    pathway_counts = dict()
-    for k in pathway_dict.keys():
-        count = 0
-        genes = pathway_dict[k][1]
-        for gene in genes:
-            try:
-                count += snp_counts[gene][0]
-            except KeyError:
-                pass
-        pathway_counts[k] = count
-    return pathway_counts
 
 def promoter_snps(promoter_offset=500):
     tagFiles = ["aligned_s_8_%s.vcf" % x for x in ['ATCACG','CGATGT','TTAGGC','TGACCA', 'ACAGTG', 'GCCAAT', 'CAGATC', 'ACTTGA']]
@@ -179,7 +123,8 @@ if __name__ == '__main__':
     for k,v in pathway_dict.items():
         pathway_dict[k] = v[1] # only keep the gene list
         
-    (annodb, al, dna, snps, gsd) = load_data(tagFiles)
+    annodb, al, dna = read_genbank_annots('NC_000913.gbk')
+    snps = read_tag_files(tagFiles)
     results = analyze_nonsyn_groups(pathway_dict, snps, annodb, al, dna)
     for k, v in results:
         print "%s,%s,%s" % (k, v, " ".join(pathway_dict[v][1]))
