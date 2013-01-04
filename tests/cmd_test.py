d4fa8ca5184673aa16c44c1207f93315398a8a36
Christopher Lee <leec@chem.ucla.edu>
2013-01-04 13:19:33 -0800

    trivial command line tests, just checks if they die!

---
+++ tests/cmd_test.py
@@ -0,0 +1,32 @@
+import unittest
+import subprocess
+import glob
+
+class Command_Test(unittest.TestCase):
+    'trivial command line tests; just checks whether they die!'
+
+    def setUp(self, vcfPattern='data/aligned_*.vcf'):
+        self.vcfFiles = glob.glob(vcfPattern)
+        assert len(self.vcfFiles) > 0
+
+    def test_analyze(self):
+        subprocess.check_output(['phenoseq_analyze', '-g', 'data/NC_000913.gbk']
+                                + self.vcfFiles)
+
+    def test_cost(self):
+        subprocess.check_output(['phenoseq_cost', '20', '30', '1000',
+                                 '50', '5'])
+
+    def test_pathways(self):
+        subprocess.check_output('phenoseq_pathways -g data/NC_000913.gbk -f data/func-associations.col -t data/genes.col'.split()
+                                + self.vcfFiles)
+
+    def test_kaks(self):
+        subprocess.check_output('phenoseq_kaks -g data/NC_000913.gbk -f data/func-associations.col -t data/genes.col'.split()
+                                + self.vcfFiles)
+
+    def test_hypergeom(self):
+        subprocess.check_output('phenoseq_hypergeom -n 30 -g data/NC_000913.gbk -f data/func-associations.col -t data/genes.col'.split()
+                                + self.vcfFiles)
+
+        
